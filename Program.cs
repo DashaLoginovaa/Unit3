cfd53e0aeaa54a216e7d759ea5ec27c8eb510129
SkillFactoryCSharpTest <dashulichka311214@gmail.com>
2024-01-25 12:46:33 +0700

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Модуль3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name:");
+            var Name = Console.ReadLine();
+            Console.WriteLine("Enter your age:");
+            var age = Console.ReadLine();
+            Console.WriteLine($"Your name is {Name} and age is {age}");
+            Console.WriteLine("Enter your birthdate:");
+            var birthdate = Console.ReadLine();
+            Console.WriteLine($"Your birthdate is {birthdate}");
+            Console.ReadKey();
+
+        }
+    }
+}
